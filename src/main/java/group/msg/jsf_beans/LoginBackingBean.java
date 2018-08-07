bbb5deb717b499cf459e18326a180b15060222dc
gnicoleta <nicoleta.gafitanu@gmail.com>
2018-08-07 15:03:40 +0300

    WORKING LOGIN

--- src/main/java/group/msg/jsf_beans/LoginBackingBean.java
+++ src/main/java/group/msg/jsf_beans/LoginBackingBean.java
@@ -1,8 +1,11 @@
 package group.msg.jsf_beans;
+//import group.msg.test.jpa.JPABaseTest;
 
 import group.msg.entities.User;
 import lombok.Data;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -12,22 +15,41 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Named
 @SessionScoped
-public class LoginBackingBean implements Serializable {
+public class LoginBackingBean  implements Serializable {
     //private String username;
     private int username;
     private String pwd;
 
+    //@Inject
+    protected UserTransaction utx;
+
    // @Inject
     private UserDBOperationsBean user;
 
-    //@Inject
-    private EntityManager em;
+    @PersistenceContext
+    EntityManager em;
+
+
+    private User book;
+    private List<User> books;
+
+    @EJB
+    private T1 service;
+
+    @PostConstruct
+    public void init() {
+        book = new User();
+        //books = service.list();
+    }
 
 
 
@@ -38,17 +60,34 @@ public class LoginBackingBean implements Serializable {
         //query.setParameter(2, "mere");
        // query.executeUpdate();
 
-        TypedQuery<User> query = em.createQuery("SELECT e FROM User e WHERE e.username LIKE 'ana'", User.class);
-        List<User> list = query.getResultList();
+//        //em.joinTransaction();
+//        try {
+//            utx.begin();
+//        } catch (NotSupportedException e) {
+//            e.printStackTrace();
+//        } catch (SystemException e) {
+//            e.printStackTrace();
+//        }
+//        em.joinTransaction();
+
+
+
+        User user1 = new User();
+        user1.setFirstName("llllllll");
+
+        service.save(user1);
+
+        em.persist(user1);
+
+        Query query = em.createQuery("SELECT e FROM User e WHERE e.firstName LIKE 'llllllll'");
+        List<User> list = (List<User>) query.getResultList();
 
         System.out.println("EX1");
         for (User i : list) {
-            System.out.println("Student: " + i.getId() + " is from city: "+ i.getUsername() );
+            System.out.println("USER: " + i.getId() + " has username: "+ i.getUsername() );
         }
 
-        User user1 = new User();
-        user1.setLastName("llllllll");
-        em.persist(user1);
+
 
 
         user.insertUser("ana", "maria");
