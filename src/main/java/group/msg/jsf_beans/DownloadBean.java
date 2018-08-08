4a8b2c690856050aad82de2407f74fa5c523ce4b
stanca <stanciu_alin_marius@yahoo.com>
2018-08-08 14:40:35 +0300

    downloadPDF

---
+++ src/main/java/group/msg/jsf_beans/DownloadBean.java
@@ -0,0 +1,19 @@
+package group.msg.jsf_beans;
+
+import lombok.Data;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Data
+@Named
+@SessionScoped
+public class DownloadBean implements Serializable {
+
+    @Inject
+    private Download download;
+
+
+}
