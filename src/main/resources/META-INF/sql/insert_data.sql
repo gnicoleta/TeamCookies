cbf3aa0d1052d22375802a356bacce9c5861fb52
stanca <stanciu_alin_marius@yahoo.com>
2018-08-17 13:06:14 +0300

    all working-final

--- src/main/resources/META-INF/sql/insert_data.sql
+++ src/main/resources/META-INF/sql/insert_data.sql
@@ -16,17 +16,17 @@ insert into jbugger.user (ID, FIRST_NAME, LAST_NAME, EMAIL, MOBILENUMBER, PASSWO
 
 insert into jbugger.user (ID, FIRST_NAME, LAST_NAME, EMAIL, MOBILENUMBER, PASSWORD, USERNAME, USERSTATUS) VALUES (130,'admin','admin','admin@msggroup.com', '+40456789011', 'admin', 'admin', 0);
 
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (100, "Baby Bee", "Bad JSON data coming back", "2.0", 100, NOW(), 1, 130, 0);
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (101, "Cuddle Bug", "Object is not displayed in rviz after using kinect detection", "3.1", 101, NOW(), 2, 130, 0);
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (102, "June Bug", "Problem while executing image_recognition.py", "1.0", 110, NOW(), 0, 130, 0);
-
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (103, "Cute As A Bug", "Angular tests broken / unimplemented in client-rest/", "4.1", 200, NOW(), 3, 130, 0);
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (110, "Don't Bug Me, I'm Busy", "IntelliJ reports error with duplicate AutoValue generated classes", "5.2", 300, NOW(), 1, 130, 0);
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (120, "June Bug", "Rolify - errors with polymorphic associations", "6.0", 120, NOW(), 2, 130, 0);
-
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (140, "Here's The Buzz", "Angular tests broken / unimplemented in client-rest/", "4.1", 100, NOW(), 0, 130, 0);
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (200, "If You Love Something, Set It Free", "PG::DatatypeMismatch: ERROR: argument of AND must be type boolean, not type record", "7.2", 200, NOW(), 2, 130, 0);
-insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (300, "Itsy Bitsy Spider", "Uninitialized constant Rolify (NameError)", "6.1", 300, NOW(), 1, 130, 0);
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (100, "Baby Bee", "Bad JSON data coming back", "2.0", 100, NOW(), 1, 130, 0);
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (101, "Cuddle Bug", "Object is not displayed in rviz after using kinect detection", "3.1", 101, NOW(), 2, 130, 0);
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (102, "June Bug", "Problem while executing image_recognition.py", "1.0", 110, NOW(), 0, 130, 0);
+--
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (103, "Cute As A Bug", "Angular tests broken / unimplemented in client-rest/", "4.1", 200, NOW(), 3, 130, 0);
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (110, "Don't Bug Me, I'm Busy", "IntelliJ reports error with duplicate AutoValue generated classes", "5.2", 300, NOW(), 1, 130, 0);
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (120, "June Bug", "Rolify - errors with polymorphic associations", "6.0", 120, NOW(), 2, 130, 0);
+--
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (140, "Here's The Buzz", "Angular tests broken / unimplemented in client-rest/", "4.1", 100, NOW(), 0, 130, 0);
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (200, "If You Love Something, Set It Free", "PG::DatatypeMismatch: ERROR: argument of AND must be type boolean, not type record", "7.2", 200, NOW(), 2, 130, 0);
+-- insert into jbugger.bug (ID, TITLE, DESCRIPTION, VERSION, assigned_user, TARGET_DATE, SEVERITY, created_by, STATUSTYPE) VALUES (300, "Itsy Bitsy Spider", "Uninitialized constant Rolify (NameError)", "6.1", 300, NOW(), 1, 130, 0);
 
 insert into jbugger.role(ID, ROLE, ROLE_STRING) VALUES (100, 0,'ADM');
 insert into jbugger.role(ID, ROLE, ROLE_STRING) VALUES (200, 1,'PM');
